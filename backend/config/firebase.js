c6846e36f4adcf8f78102aaf89459e05a4414f00
Hamza525318 <hamzamustafa273@gmail.com>
2024-05-17 19:22:47 +0530

    made some modifications related to private keys

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,10 @@
 const {initializeApp,cert} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore")
+const {getFirestore} = require("firebase-admin/firestore");
+const {service_acc_cred} = require("./service_account")
 
 //Returns a credential created from the provided service account that grants admin access to Firebase services. This credential can be used in the call to firebase-admin.app#initializeApp .
 
-const serviceAccount = require("./service_account.json");
+const serviceAccount = require(JSON.stringify(service_acc_cred));
 
 initializeApp({
     credential: cert(serviceAccount),
